8d5ad5ed117b48189ea7c8a2165fcfe20fe024fa
Kacper <kacpercybulski@icloud.com>
2024-04-13 18:07:13 +0200

    first desing is almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 function App() {
 	return (
 		<>
-			<h2>hey</h2>
+			<header>
+				<h1 className="title">Quizzical</h1>
+				<p className="description">Are you ready for a quiz?</p>
+				<button className="start">Start quiz</button>
+			</header>
 		</>
 	)
 }

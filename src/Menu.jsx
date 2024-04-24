abc0f54150fd5b8238600da083c07bfc953e024e
Kacper <kacpercybulski@icloud.com>
2024-04-24 21:47:00 +0200

    trying to figure out why api dosent work

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -4,29 +4,6 @@ import blob2 from '/src/assets/blob2.svg'
 import { useState, useEffect } from 'react'
 
 export function Menu() {
-	const questions = [
-		{
-			question: 'How would one say goodbye in Spanish?',
-			answers: ['Adiós', 'Hola', 'Au Revoir', 'Salir'],
-		},
-		{
-			question: 'Which best selling toy of 198dddd3 caused hysteria, resulting in riots breaking in stores?',
-			answers: ['Cabbage Patch Kids', 'Transformers', 'Care Bears', 'Rubik’s Cube'],
-		},
-		{
-			question: 'What is the hottest planet in our Solar System?',
-			answers: ['Mercury', 'Venus', 'Mars', 'Saturn'],
-		},
-		{
-			question: 'In which country was dadadthe caesar salad invented?',
-			answers: ['Italy', 'Portugal', 'Maaaxico', 'France'],
-		},
-		{
-			question: 'How Many Hearts Does An dada Have?',
-			answers: ['One', 'Two', 'Three', 'Four'],
-		},
-	]
-
 	const [quizApi, setQuizApi] = useState([])
 
 	useEffect(() => {
@@ -43,33 +20,14 @@ export function Menu() {
 			<div className="game-container">
 				<div className="game">
 					<div className="qa">
-						{quizApi.map((questionObj, index) => (
+						{quizApi.map((question, index) => (
 							<div key={index}>
-								<h2 className="question">{questionObj.question}</h2>
-								{questionObj.answers.map((answers, answersIndex) => (
-									<button key={answersIndex} className="answer">
-										{answers}
-									</button>
-								))}
+								<h2>{question.question}</h2>
+								<button>{question.correct_answer}</button>
 
 								{index !== questions.length - 1 && <hr />}
 							</div>
 						))}
-
-						{/* {quizApi.map((questionObj, index) => (
-							<div key={index}>
-								<h2 className="question">{questionObj.question}</h2>
-								<div className="answers">
-									{questionObj.incorrect_answers.map((answer, answerIndex) => (
-										<button key={answerIndex} className="answer">
-											{answer}
-										</button>
-									))}
-									<button className="answer">{questionObj.correct_answer}</button>
-								</div>
-								{index !== quizApi.length - 1 && <hr />}
-							</div>
-						))} */}
 
 						<span className="final-result">
 							You scored 3/5 correct answers

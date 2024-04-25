c68123409eb50286c92192f8115670f256ca29eb
Kacper <kacpercybulski@icloud.com>
2024-04-25 11:32:55 +0200

    data is being displayed from api

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -4,33 +4,66 @@ import blob2 from '/src/assets/blob2.svg'
 import { useState, useEffect } from 'react'
 
 export function Menu() {
-	const [quizApi, setQuizApi] = useState([])
+	const [questions, setQuestions] = useState([])
+	const [questionsAndAnswers, setQeustionsAndAnswers] = useState([])
 
 	useEffect(() => {
 		fetch('https://opentdb.com/api.php?amount=5')
 			.then(res => res.json())
-			.then(data => setQuizApi(data.results))
-			.catch(error => console.error('Error while fetching the quiz data'), error)
+			.then(data => {
+				setQuestions(data.results)
+				setQeustionsAndAnswers(
+					data.results.map(questionObj => {
+						return {
+							question: questionObj.question,
+							shuffledAnswers: shuffle([...questionObj.incorrect_answers, questionObj.correct_answer]),
+							correctAnswer: questionObj.correctAnswer,
+							selectedAnswer: '',
+						}
+					})
+				)
+			})
 	}, [])
 
-	console.log(quizApi)
+	// function to shuffle answers
+	function shuffle(array) {
+		let currentIndex = array.length
+
+		// While there remain elements to shufle...
+		while (currentIndex != 0) {
+			// Pick a remaining element...
+			let randomIndex = Math.floor(Math.random() * currentIndex)
+			currentIndex--
+
+			// And swap it with the current element.
+			;[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]]
+		}
+		return array
+	}
+
+	console.log(questionsAndAnswers)
 
 	return (
 		<>
 			<div className="game-container">
 				<div className="game">
 					<div className="qa">
-						{quizApi.map((question, index) => (
+						{questionsAndAnswers.map((question, index) => (
 							<div key={index}>
 								<h2>{question.question}</h2>
-								<button>{question.correct_answer}</button>
 
-								{index !== questions.length - 1 && <hr />}
+								{question.shuffledAnswers.map((answers, answersIndex) => (
+									<button className="answer" key={answersIndex}>
+										{answers}
+									</button>
+								))}
+
+								{index !== questionsAndAnswers.lenghth - 1 && <hr />}
 							</div>
 						))}
 
 						<span className="final-result">
-							You scored 3/5 correct answers
+							{/* You scored 3/5 correct answers */}
 							<button className="check-answer">Check answers</button>
 						</span>
 					</div>
